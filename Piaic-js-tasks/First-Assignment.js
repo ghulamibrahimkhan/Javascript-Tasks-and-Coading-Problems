6f68b16eb8fe7919d55c765e3eb4cc1bfa44b69d
Ghulam Ibrahim Khan <gulamibrahimkhan@gmail.com>
2022-08-11 14:05:05 +0500

    First assignment, Piaic js video series

---
+++ Piaic-js-tasks/First-Assignment.js
@@ -0,0 +1,109 @@
+// *** 1
+// alert("Welcome to Js Land...\nHappy coading");
+
+// *** 2
+// student_name = "Imran Khan";
+// age = 18;
+// subject = "blockchain";
+// alert(student_name);
+// alert(age + " Years old");
+// alert(subject);
+
+// *** 3
+// email = "example@gmail.com";
+// alert("My email address is " + email)
+
+// *** 4
+// document.write("Yeas I can write HTML content through JavaScript").
+
+// *** 5
+// age = 18;
+// alert("I'm 15 years old");
+
+//  *** 6
+// birthYear = 2002;
+// document.write("My birth year is " + birthYear + "<br/>" + "Data type of Declared variable is number");
+
+//  *** 7
+// document.write("<h1>Rules for naming JS variables</h1>");
+// document.write(" Variables names can only contain numbers,$ and _ . For example :$my_1stVariable <br>");
+// document.write("Variables must begin with a letter, $ or _ . For example : $name, _name or name <br>");
+// document.write("Variables names are case sensetive <br>");
+// document.write("Variables names should not be JS Keywords"); 
+
+// *** 8
+// num1 = 3
+// num2 = 5
+// sum = num1 + num2;
+// alert("The sum of 3 + 5 is " + sum);
+
+// *** 9
+// num1 = 3
+// num2 = 5
+// // sub
+// sub = num1 - num2;
+// alert("The sub of 3 - 5 is " + sub);
+// // Multi
+// multi = num1 * num2;
+// alert("The Multiplication of 3 * 5 is " + multi);
+// // Divide
+// divide = num1 / num2;
+// alert("The divide of 3 + 5 is " + divide);
+// // Mod
+// mod = num1 % num2;
+// alert("The mod of 3 % 5 is " + mod);
+
+// *** 10
+// var value;
+// document.write("Value after variable declaration is " + value);
+// value = 5;
+// document.write("<br> Initial Value: " + value);
+// value++;
+// document.write("<br> Value after increament is: " + value );
+// value += 7;
+// document.write("<br> Value after addition is: " + value);
+// value--;
+// document.write("<br> Value after decrement is: " + value);
+// value %= 3;
+// document.write("<br> The remainder is: " + value);
+
+// *** 11
+// 11.1
+// // Celsius to Fahrenheit
+//Formula: C = (F - 32 ) x 5 / 9;
+// Celsius =  25;
+// Fahrenheit = (Celsius * ( 9 / 5 )) + 32
+// document.write(Celsius + "C is " + Fahrenheit + "F <br>"); 
+
+// 11.2
+// Fahrenheit to Celsius
+// Formula: F = (C x 9 / 5 ) +32;
+// Fahrenheit = 70;
+// Celsius = (Fahrenheit - 32) * 5 / 9
+// document.write(Fahrenheit + "F is " + Celsius + "C");
+
+// 12
+// pkr = (10 * 155 ) + (25 *41 );
+// document.write("<h1>Currency in Pkr</h1> <br> Total Currency in pkr " + pkr);
+
+// 13
+num = 10;
+// document.write("Result <br> The value of num is: " + num + "<br>............................<br><br>");
+// ++num
+// document.write("The value of ++num is: " + num + "<br>");
+// document.write("Now value of num is: " + num + "<br><br><br>");
+// x = num++
+// document.write("The value of num++ is: " + x + "<br>");
+// document.write("Now value of num is: " + num + "<br><br><br>");
+// --num
+// document.write("The value of --num is: " + num + "<br>");
+// document.write("Now value of num is: " + num + "<br><br><br>");
+// x = num--
+// document.write("The value of ++num is: " + x + "<br>");
+// document.write("Now value of num is: " + num + "<br>");
+
+// 14
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// //             1  -  0 +  1  + 1
+// document.write("a is " + a + "<br> b is " + b + "<br> result is " + result)
